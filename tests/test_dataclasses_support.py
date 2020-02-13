071559cded971075374f34dc20144c7a8408d5d0
ivan <ivan.prado@gmail.com>
2020-02-13 16:56:29 +0000

    Minimum required version for dataclasses support is 3.7.6

--- tests/test_dataclasses_support.py
+++ tests/test_dataclasses_support.py
@@ -13,16 +13,16 @@ def test_dataclasses_py36():
     assert andi.inspect(BDC.__init__) == {'a': [ADC]}
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7),
-                    reason="Dataclasses require Python 3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 7, 6),
+                    reason="Dataclasses require Python 3.7.6 or higher")
 def test_dataclasses_py37():
     from py37 import ADC, BDC
     assert andi.inspect(ADC.__init__) == {'b': [BDC]}
     assert andi.inspect(BDC.__init__) == {'a': [ADC]}
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7),
-                    reason="Dataclasses require Python 3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 7, 6),
+                    reason="Dataclasses require Python 3.7.6 or higher")
 def test_dataclasses_py37_no_pep_563():
     from py37_no_pep_563 import ADC, BDC
     assert andi.inspect(ADC.__init__) == {'b': [BDC]}
@@ -30,7 +30,7 @@ def test_dataclasses_py37_no_pep_563():
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7),
-                    reason="Dataclasses require Python 3.7 or higher")
+                    reason="Dataclasses require Python 3.7.6 or higher")
 def test_dataclasses_py37_str_ref():
     """ String annotations are returned as ForwardRef when
     ``from __future__ import annotations`` is used. Just don declare them as string. """
