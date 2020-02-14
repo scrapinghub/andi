f773dc5ba86d8cc84cfe596b755ad74387398582
ivan <ivan.prado@gmail.com>
2020-02-14 11:13:55 +0000

    Group forward references together

--- tests/test_dataclasses_support.py
+++ tests/test_dataclasses_support.py
@@ -13,24 +13,28 @@ def test_dataclasses_py36():
     assert andi.inspect(BDC.__init__) == {'a': [ADC]}
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7, 6),
-                    reason="Dataclasses require Python 3.7.6 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 7),
+                    reason="Dataclasses require Python 3.7 or higher")
 def test_dataclasses_py37():
     from py37 import ADC, BDC
-    assert andi.inspect(ADC.__init__) == {'b': [BDC]}
     assert andi.inspect(BDC.__init__) == {'a': [ADC]}
+    from py37_no_pep_563 import ADCnp, BDCnp
+    assert andi.inspect(BDCnp.__init__) == {'a': [ADCnp]}
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7, 6),
-                    reason="Dataclasses require Python 3.7.6 or higher")
-def test_dataclasses_py37_no_pep_563():
-    from py37_no_pep_563 import ADC, BDC
+                    reason="Dataclasses with forward references require "
+                           "Python 3.7.6 or higher")
+def test_dataclasses_py37_forward_refs():
+    from py37 import ADC, BDC
     assert andi.inspect(ADC.__init__) == {'b': [BDC]}
-    assert andi.inspect(BDC.__init__) == {'a': [ADC]}
+    from py37_no_pep_563 import ADCnp, BDCnp
+    assert andi.inspect(ADCnp.__init__) == {'b': [BDCnp]}
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7),
-                    reason="Dataclasses require Python 3.7.6 or higher")
+                    reason="Dataclasses with forward references require "
+                           "Python 3.7.6 or higher")
 def test_dataclasses_py37_str_ref():
     """ String annotations are returned as ForwardRef when
     ``from __future__ import annotations`` is used. Just don declare them as string. """
