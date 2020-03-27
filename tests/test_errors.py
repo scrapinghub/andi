b5546887cb519b884e47f5261b9a5aea6960be44
ivan <ivan.prado@gmail.com>
2020-03-27 16:33:37 +0000

    Restore test coverage

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,6 +1,9 @@
-import andi
+import pytest
+
 from andi.errors import CyclicDependencyErrCase, NonInjectableOrExternalErrCase, \
-    LackingAnnotationErrCase, _class_or_func_str
+    LackingAnnotationErrCase, _class_or_func_str, _cyclic_dependency_error, \
+    _argument_lacking_annotation_error, _no_injectable_or_external_error, \
+    _exception_msg
 from tests.test_plan import E, A
 
 
@@ -8,13 +11,16 @@ def test_error_messages():
     cyc = CyclicDependencyErrCase(E, [E, A, E])
     no_inj = NonInjectableOrExternalErrCase('args', E, [A,E])
     lack = LackingAnnotationErrCase('arg', E)
-    assert type(andi.errors._cyclic_dependency_error(*cyc)) == str
-    assert type(andi.errors._argument_lacking_annotation_error(*lack)) == str
-    assert type(andi.errors._no_injectable_or_external_error(*no_inj)) == str
-    assert type(andi.errors._exception_msg(E, {
+    assert type(_cyclic_dependency_error(*cyc)) == str
+    assert type(_argument_lacking_annotation_error(*lack)) == str
+    assert type(_no_injectable_or_external_error(*no_inj)) == str
+    assert type(_exception_msg(E, {
         'a': [cyc, no_inj],
         'b': [lack, cyc],
     })) == str
+
+    with pytest.raises(Exception):
+        _exception_msg(E, {'arg': tuple('a', 'b')})
 
 
 def test_class_or_func_str():
